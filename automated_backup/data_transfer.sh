0dda0e8c94ae1fdf37c25c714420cf4126b8b2c0
Harshit gupta <onxy.harsh123@gmail.com>
2025-08-20 13:36:30 +0530

    rsync failure resolved as well as added retry logic for rsync

--- automated_backup/data_transfer.sh
+++ automated_backup/data_transfer.sh
@@ -1,46 +1,119 @@
 #!/bin/bash
 
-ip="<server-private-ip>"
-instanceid="<instance-id>"
-logfile="<logfilelocation>"
+path="/home/ubuntu/data/cpu_data.txt"
+log="/home/ubuntu/logs/res_log.txt"
+key="/home/ubuntu/keys/key2.pem"
+rsync="/usr/bin/rsync"
 
-date=$(date '+%Y%m%d_%H%M%S')
+ip=""
+instance=""
 
-aws ec2 start-instances --instance-ids "${instanceid}"
+date=$(date '+%Y-%m-%d')
 
-aws ec2 wait instance-status-ok --instance-ids "${instanceid}"
+loc="/home/ubuntu/scripts/automated_datasender"
+file="${loc}/data_${date}.enc"
 
-data="backup_${date}.gz"
+aws ec2 start-instances\
+        --instance-ids "${instance}"
 
-gzip -c /home/ubuntu/prot1data.txt > /home/ubuntu/"${data}"
+echo "starting the server $(date)" >> "${log}"
 
-rsync -avz -e "ssh -i /home/ubuntu/key/key1.pem -o StrictHostKeyChecking=accept-new" /home/ubuntu/"${data}" ubuntu@"${ip}":/home/ubuntu/backups
+aws ec2 wait instance-status-ok --instance-ids "${instance}"
 
-status=$?
+stat=$(aws ec2 describe-instance-status \
+    --instance-ids "${instance}" \
+    --query 'InstanceStatuses[*].[SystemStatus.Status,InstanceStatus.Status]' \
+    --output text | tr -s '\t' ' ')
 
-        if [ $status -eq 0 ]; then
 
-                echo "data sended to server" >> /home/ubuntu/logs/"${logfile}"
+if [[ $stat == "ok ok" ]]; then
 
-        else
 
-                echo " data cant be send $(date)" >> /home/ubuntu/logs/"${logfile}"
+
+        echo "server is running $(date)" >> "${log}"
+
+fi
+openssl enc -aes-256-cbc -salt -pbkdf2 -iter 100000 -in "${path}" -out "${file}" -base64 -pass pass:Harshit@1234
+
+
+
+count=1
+max_try=12
+
+echo "starting to testing the ssh connection $(date)" >> "${log}"
+
+while true; do
+
+
+        if ssh -i "${key}" -o StrictHostKeyChecking=accept-new ubuntu@"${ip}" 'echo ssh ready' 2>/dev/null; then
+
+
+
+                echo "ssh is ready in ${count} time $(date)" >> "${log}"
+
+                break
 
         fi
 
+        ((count++))
 
 
-echo "server or port is unreachable at $(date)" >>/home/ubuntu/logs/"${logfile}"
 
-sleep 5
+        if [ $count -gt $max_try ]; then
 
-
-> /home/ubuntu/prot1data.txt
-
-rm /home/ubuntu/"${data}"
+                echo "ssh is having problem to connect $(date)" >> "${log}"
+                exit 1
 
 
-aws ec2 stop-instances --instance-ids "${instanceid}"
 
-echo "server stopped" >> /home/ubuntu/logs/"${logfile}"
-                                                                                  
+        fi
+
+        sleep 10
+
+done
+
+echo "connection checked and its active $(date)" >> "${log}"
+
+rsync_count=1
+max_retry=5
+
+while true; do
+
+
+        "${rsync}" -avz --progress -e "ssh -i ${key} -o StrictHostKeyChecking=accept-new" "${file}" ubuntu@"${ip}":/home/ubuntu/backups
+
+        status=$?
+
+
+        if [ "$status" -eq 0 ]; then
+
+
+                echo "data is transfered over server ${ip} at ${rsync_count} times $(date)" >> "${log}"
+                break
+
+        elif [ "$rsync_count" -gt "$max_retry" ]; then
+
+                echo " Rsync cant be done after 5 retries $(date) " >> "${log}"
+
+                break
+        else
+                echo "data is not transfered its try no ${rsync_count} $(date)" >> "${log}"
+
+
+
+        ((rsync_count++))
+        fi
+
+        sleep 5
+done
+
+> "${path}"
+rm -f "${file}"
+
+
+echo "data is cleared as well $(date)" >> "${log}"
+
+aws ec2 stop-instances\
+        --instance-ids "${instance}"
+
+echo "server stopped $(date)" >> "${log}"                                                                 
